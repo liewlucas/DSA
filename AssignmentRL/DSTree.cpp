fc3dbd407ef8813f0d1b60cd5f294f78f655959e
Lucas Liew <s10179187@connect.np.edu.sg>
2019-01-23 18:01:31 +0800

    Added Data Structure Class

---
+++ AssignmentRL/DSTree.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "DSTree.h"
+
+
+DSTree::DSTree()
+{
+}
+
+
+DSTree::~DSTree()
+{
+}
---
+++ AssignmentRL/DSTree.h
@@ -0,0 +1,8 @@
+#pragma once
+class DSTree
+{
+public:
+	DSTree();
+	~DSTree();
+};
+
