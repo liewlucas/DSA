71b61901e89673e2c3b86c48ce6f7aaf70edc36f
Lucas Liew <s10179187@connect.np.edu.sg>
2019-01-23 22:12:03 +0800

    started codes for class files

--- AssignmentRL/DSTree.cpp
+++ AssignmentRL/DSTree.cpp
@@ -7,6 +7,3 @@ DSTree::DSTree()
 }
 
 
-DSTree::~DSTree()
-{
-}
--- AssignmentRL/DSTree.h
+++ AssignmentRL/DSTree.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std; 
+
+const int CHAR_SIZE = 128;
+
 class DSTree
 {
 public:
+	bool isleaf;
+	DSTree*character[CHAR_SIZE];
+
+	//CONSTRUCTOR 
 	DSTree();
-	~DSTree();
+
+	//INSERT NEW WORD INTO DICTIONARY
+	void insert(const string);
+
+	//SEARCH FROM DICTIONARY
+	bool search(const string);
+
+	//display all words from user input
+	void searchbour (DSTree*, const string)
 };
 
