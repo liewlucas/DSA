3354670fd52f13ff8ac3c1327fa42cb05eacaa4f
Jianwen Rayner Tan <s10177641@connect.np.edu.sg>
2019-01-24 12:33:23 +0800

    Implemented check word/ check file

--- AssignmentRL/DSTree.cpp
+++ AssignmentRL/DSTree.cpp
@@ -4,9 +4,59 @@
 
 DSTree::DSTree()
 {
+	// Initialise Lead node to false
+	isLeaf = false;
+	for (int i = 0; i < CHAR_SIZE; i++)
+	{
+		alphabet[i] = nullptr; // set letter to null since no letter has been initiated in the tree
+	}
 }
 
-
-DSTree::~DSTree()
+void DSTree::insert(string word)
 {
+	// Create pointer pointing to address of root node (this)
+	DSTree* curr = this;
+	for (int i = 0; i < word.length(); i++)
+	{
+		// Create node if path not found
+		if (curr->alphabet[word[i]] == nullptr) // If address of index Letter is null
+		{
+			curr->alphabet[word[i]] = new DSTree(); // Sets index Letter of character array to new DS
+		}
+		curr = curr->alphabet[word[i]]; // Traverse from root to inserted word
+	}
+
+	// set current node as leaf
+	curr->isLeaf = true;
 }
+bool DSTree::search(const string word)
+{
+	
+	if (this == nullptr)
+	{
+		return false; // If tree is empty, return false
+	}
+
+	DSTree* curr = this; // Get root address
+	for (int i = 0; i < word.length(); i++)
+	{
+		// traverse to next node
+		curr = curr->alphabet[word[i]];
+
+		if (curr == nullptr) // If next node is null, meaning word not registered
+		{
+			return false;
+		}
+	}
+	return curr->isLeaf; // If entire word is found, last letter->isLeaf would return true
+}
+
+
+
+
+// Part to edit & Understand
+
+
+
+
+
--- AssignmentRL/DSTree.h
+++ AssignmentRL/DSTree.h
@@ -1,8 +1,25 @@
 #pragma once
-class DSTree
-{
-public:
-	DSTree();
-	~DSTree();
-};
+#include <iostream>
+#include <string>
+using namespace std;
 
+const int CHAR_SIZE = 128;
+
+//DSTREEEEE
+class DSTree {
+private:
+public:
+	bool isLeaf;
+	DSTree* alphabet[CHAR_SIZE];
+
+	// Contructor
+	DSTree();
+	// Insert header
+	void insert(const string);
+	// Search header
+	bool search(const string);
+	// Display all words from closest input
+	const void searchClosest(DSTree*, const string);
+	// Traverse through tree
+	void traverse(string, DSTree*);
+};
